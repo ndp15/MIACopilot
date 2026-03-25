9cdab9fc74e5965a9d1a55f4f6083f170de371c5
Noa De Paola <noa@depaola.ch>
2026-03-25 13:19:40 +0100

    Delete src/ApprenticeManagement directory

--- src/ApprenticeManagement/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApprenticeManagement.Repositories;
-using ApprenticeManagement.Services;
-using ApprenticeManagement.UI;
-
-var repository = new ApprenticeRepository();
-var service = new ApprenticeService(repository);
-var menu = new ConsoleMenu(service);
-
-menu.Run();
